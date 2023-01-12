4dcb6f7b3013eec790d2020b2907e6ffdfac4b33
Katiix <katuha1995@gmail.com>
2023-01-12 19:33:19 +0200

    login

---
+++ src/test/java/tests/Login.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Login extends TestBase{
+
+    @BeforeMethod
+    public void preConditions(){
+        if(app.getUser().isLogged()){
+            app.getUser().logOut();
+        }
+    }
+
+
+
+    @Test
+    public void login1(){
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillInLoginForm("katuha1995@gmail.com","12345.com");
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+        Assert.assertTrue(app.getUser().isLogged());
+    }
+
+
+
+
+}
